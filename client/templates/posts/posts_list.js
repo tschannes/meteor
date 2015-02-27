666d32fa942735708aee20e54ded6437e6eb42f8
tschannes <mail@tscherrig.net>
2015-02-27 13:38:58 +0100

    added basic routing

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,21 +1,3 @@
-/*var postsData = [
-  {
-    title: 'Introducing Telescope',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  }, 
-  {
-    title: 'Meteor',
-    url: 'http://meteor.com'
-  }, 
-  {
-    title: 'The Meteor Book',
-    url: 'http://themeteorbook.com'
-  },
-  {
-    title: 'Hannes Tscherrig',
-    url: 'http://tschannes.ch'
-  }
-];*/
 Template.postsList.helpers({
   posts: function(){
     return Posts.find();
