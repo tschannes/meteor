619e09b74362b14750170d9f46544d2d72867f77
tschannes <mail@tscherrig.net>
2015-02-27 16:50:24 +0100

    added duplicate check and description

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,12 +1,25 @@
 Template.postSubmit.events({
-	'submit form':function(e){
+	'submit form': function(e) {
 		e.preventDefault();
 		var post = {
-			url:$(e.target).find('[name=url]').val(),
-			title:$(e.target).find('[name=title]').val()
+			url: $(e.target).find('[name=url]').val(),
+			title: $(e.target).find('[name=title]').val(),
+			description: $(e.target).find('[name=description]').val()
 		};
 
-		post._id = Posts.insert(post);
-		Router.go('postPage', post);
+		Meteor.call('postInsert', post, function(error, result) {
+			if (error) {
+				return alert(error.message);
+			}
+			// show this result but route anyway
+			if (result.postExists)
+				alert('This link has already been posted');
+
+			Router.go('postPage', {
+				_id: result._id
+			});
+		});
+
+		/*post._id = Posts.insert(post);*/
 	}
-})
+});
