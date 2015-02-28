1a5531125922d59fb3eec737436dea19db13bf77
tschannes <mail@tscherrig.net>
2015-02-28 20:02:30 +0100

    fixed single post view with a single post subscription

--- server/publications.js
+++ server/publications.js
@@ -7,6 +7,11 @@ Meteor.publish('posts',function(options){
   return Posts.find({}, options);
 });
 
+Meteor.publish('singlePost', function(id) {
+  check(id, String)
+  return Posts.find(id);
+});
+
 /*Meteor.publish('comments', function() {
   return Comments.find();
 });*/
