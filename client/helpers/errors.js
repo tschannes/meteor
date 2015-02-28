151c7bf5ade44665c9919742aa93c7d828ea1552
tschannes <mail@tscherrig.net>
2015-02-28 16:58:23 +0100

    added validation to submit forms

---
+++ client/helpers/errors.js
@@ -0,0 +1,20 @@
+Errors = new Mongo.Collection(null);
+
+throwError = function(message) {
+	Errors.insert({
+		message: message
+	});
+};
+
+Template.errors.helpers({
+	errors: function() {
+		return Errors.find();
+	}
+});
+
+Template.error.rendered = function() {
+  var error = this.data;
+  Meteor.setTimeout(function () {
+    Errors.remove(error._id);
+  }, 3000);
+};
--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,16 @@
+Template.postSubmit.created = function() {
+	Session.set('postSubmitErrors', {});
+};
+
+Template.postSubmit.helpers({
+	errorMessage: function(field) {
+		return Session.get('postSubmitErrors')[field];
+	},
+	errorClass: function(field) {
+		return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+	}
+});
+
 Template.postSubmit.events({
 	'submit form': function(e) {
 		e.preventDefault();
@@ -7,13 +20,19 @@ Template.postSubmit.events({
 			description: $(e.target).find('[name=description]').val()
 		};
 
+		var errors = validatePost(post);
+		if (errors.title || errors.url || errors.description)
+			return Session.set('postSubmitErrors', errors);
+
 		Meteor.call('postInsert', post, function(error, result) {
 			if (error) {
-				return alert(error.message);
+				/*return alert(error.message);*/
+				return throwError(error.reason);
 			}
 			// show this result but route anyway
 			if (result.postExists)
-				alert('This link has already been posted');
+			/*alert('This link has already been posted');*/
+				throwError('This link has already been posted!');
 
 			//REDIRECTING
 			Router.go('postPage', {
