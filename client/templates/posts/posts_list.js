94dac4bc52d085efba09788c31feb41c078324ac
tschannes <mail@tscherrig.net>
2015-03-01 14:03:42 +0100

    added page level animations

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -6,6 +6,12 @@
 */
 Template.postsList.rendered = function () {
   this.find('.wrapper')._uihooks = {
+  	insertElement: function (node, next) {
+      $(node)
+        .hide()
+        .insertBefore(next)
+        .fadeIn();
+    },
     moveElement: function (node, next) {
       var $node = $(node), $next = $(next);
       var oldTop = $node.offset().top;
@@ -39,6 +45,11 @@ Template.postsList.rendered = function () {
       // reset everything to 0, animated
       $node.addClass('animate').css('top', 0);
       $inBetween.addClass('animate').css('top', 0);
+    },
+    removeElement: function(node) {
+      $(node).fadeOut(function() {
+        $(this).remove();
+      });
     }
   }
 }
